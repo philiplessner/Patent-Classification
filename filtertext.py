0028684a44bc5213295bfffeaeee75b27c84f40f
dpmlto1 <philiplessner@kemet.com>
2014-02-22 21:12:30 -0500

    moved pipe and associated functions to utils.py

--- filtertext.py
+++ filtertext.py
@@ -3,31 +3,7 @@ from __future__ import print_function, unicode_literals
 import os
 import io
 import re
-from itertools import starmap
-from functools import partial
-
-
-def compose_two(g, f):
-    '''
-    Function composition for two functions,
-    e.g. compose_two(f, g)(x) == f(g(x))
-     '''
-    return lambda *args, **kwargs: g(f(*args, **kwargs))
-
-
-def compose(*funcs):
-    '''
-    Compose an arbitrary number of functions
-    left-to-right passed as args
-    '''
-    return reduce(compose_two, funcs)
-
-
-def transform_args(func, transformer):
-    return lambda *args: func(*transformer(args))
-
-composed_partials = transform_args(compose, partial(starmap, partial))
-pipe = transform_args(composed_partials, reversed)
+from utils import pipe
 
 
 def gen_filelist(top):
