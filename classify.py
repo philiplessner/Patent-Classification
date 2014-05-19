0cf561fd7cbc3cf0b494e63b338ef8b7985605fa
dpmlto1 <philiplessner@kemet.com>
2014-05-19 17:29:52 -0400

    Added function to compute class probabilities

--- classify.py
+++ classify.py
@@ -1,11 +1,15 @@
 from __future__ import print_function, division, unicode_literals
 from collections import defaultdict
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_files
 # from sklearn.naive_bayes import MultinomialNB
-from sklearn.linear_model import PassiveAggressiveClassifier
+# from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.linear_model import SGDClassifier
+# from sklearn.svm import SVC
+# from sklearn.grid_search import GridSearchCV
 from sklearn.cross_validation import ShuffleSplit, cross_val_score
 from sklearn.metrics import roc_curve, auc
 from sklearn.metrics import confusion_matrix
@@ -32,17 +36,18 @@ def loadClassifiedData(pathtraining):
     return dataDict
 
 
-def vector_characteristics(vectorized, transformed, feature_names,
-                           target_names):
-    print('Vectorized Shape: ', vectorized.shape)
-    print('Transformed Shape: ', transformed.shape)
-    print('Number of Categories: ', len(target_names))
-    print('\n****Target Names***\n', target_names)
-    print('Number of Features:', len(feature_names))
-    print('\n****Feature Names****\n', feature_names)
-
-
-def display_important_features(feature_names, target_names, weights, n_top=30):
+def display_important_features(feature_names, target_names, clf, n_top=30):
+    '''
+    Display highest weighted and lowest weighted feastures for each category
+    Parameters
+        feature_names: tokens vectorized
+        target_names: category names
+        clf: classifier instance
+        n_top: number of features to display
+    '''
+    fitted = clf.fit(transformed, categories)
+    weights = clf.coef_
+    print('****Feature Weights****\n', weights)
     for i, target_name in enumerate(target_names):
         print("Class: " + target_name)
         print("")
@@ -61,6 +66,17 @@ def display_important_features(feature_names, target_names, weights, n_top=30):
 
 
 def classifier_metrics(transformed, categories, clf, cv):
+    '''
+    Calculate f1 scores and confusion matrix for several cv splits
+    Parameters
+        transformed: X vector (TF-IDF for each feature/category)
+        categories: y vector
+        clf: classifier instance
+        cv: cross validation instance
+    Returns
+        cms: confusion matrix averaged over cv runs
+        f1_scores: f1 score for each cv run
+    '''
     labels = np.unique(categories)
     f1_scores = []
     cms = []
@@ -88,6 +104,11 @@ def classifier_metrics(transformed, categories, clf, cv):
 
 
 def classifier_scores(f1_scores):
+    '''
+    Prints f1 scores for cv runs and mean and std of  scores
+    Parameter
+        f1_scores: array of f1 scores for several cv runs
+    '''
     print('****F1 Test Scores****\n')
     for f1_score in f1_scores:
         print('\t {0:0.3f}'.format(f1_score))
@@ -99,8 +120,18 @@ def classifier_scores(f1_scores):
 
 
 def output_confusionmatrix(cms, categories, target_names):
+    '''
+    Prints avg confusion matrix and also plots it
+    Parameters
+        cms: array of confusion matricies from cv runs
+        categories: y vector
+        target_names: category names
+    '''
+    # Calculate average confusion matrix and normalize to fractions
     mat = np.matrix(sum(cms))
     frac = np.round(mat / mat.sum(axis=1, dtype='float'), decimals=3)
+
+    # Print normailized average confusion matrix
     print('****Confusion Matrix****\n')
     df = pd.DataFrame(dict(zip(target_names, frac.transpose())),
                       columns=target_names)
@@ -109,6 +140,7 @@ def output_confusionmatrix(cms, categories, target_names):
                             for i in range(len(target_names))]
     print(df)
 
+    # Plot confusion matrix
     fig = plt.figure(figsize=(10., 10.))
     ax = fig.add_subplot(111)
     plt.matshow(frac, fignum=False, cmap='Blues', vmin=0., vmax=1.0)
@@ -142,49 +174,76 @@ def plot_roc(auc_score, name, tpr, fpr, label=None):
     plt.show()
 
 
-# Global Constants
-PATH_REPLACE = ''.join(['/Users/dpmlto1/Documents/Patent/Thomson Innovation/',
-                        'clustering/custom/replacements.json'])
-PATH_SW = ''.join(['/Users/dpmlto1/Documents/Patent/Thomson Innovation/',
-                   'clustering/custom/combined-stop-words.txt'])
-PATH_DATA = ''.join(['/Users/dpmlto1/Documents/Patent/Thomson Innovation/',
-                     'clustering/data/new-summaries/'])
+def probs_class(patentdict, transformed, clf):
+    '''
+    Probabilities of classes for each data
+    '''
+    df_p = pd.DataFrame(clf.predict_proba(transformed),
+                        columns=patentdict['target_names'])
+    df_p['category_num'] = patentdict['target']
+    targetl = patentdict['target'].tolist()
+    targetd = dict(zip(range(len(patentdict['target_names']) + 1),
+                       patentdict['target_names']))
+    for k, v in targetd.iteritems():
+        for i, e in enumerate(targetl):
+            if k == e:
+                targetl[i] = v
+    df_p['category_name'] = targetl
+    df_p['files'] = [os.path.splitext(os.path.basename(filename))[0]
+                     for filename in patentdict['filenames']]
+    return df_p
 
 
-# Get the training and testing data
-patentdict = loadClassifiedData(PATH_DATA)
-categories = patentdict['target']
-data = patentdict['data']
+if __name__ == '__main__':
+    # Global Constants
+    PATH_REPLACE = ''.join(['/Users/dpmlto1/Documents/Patent/',
+                            'Thomson Innovation/',
+                            'clustering/custom/replacements.json'])
+    PATH_SW = ''.join(['/Users/dpmlto1/Documents/Patent/Thomson Innovation/',
+                       'clustering/custom/combined-stop-words.txt'])
+    PATH_DATA = ''.join(['/Users/dpmlto1/Documents/Patent/Thomson Innovation/',
+                         'clustering/data/new-summaries/'])
 
-data_stripped = [removeNonAscii(item) for item in data]
+    # Get the training and testing data
+    patentdict = loadClassifiedData(PATH_DATA)
+    categories = patentdict['target']
+    data = patentdict['data']
 
-# Preprocess and tokenize
-list_tokens = pp.patent_tokenizer(data_stripped,
-                                  PATH_REPLACE=PATH_REPLACE,
-                                  PATH_SW=PATH_SW)
+    data_stripped = [removeNonAscii(item) for item in data]
 
-# Vectorize
-(transformed, patenttransformer,
- vectorized, patentvectorizer) = pp.tokens_tovectors(list_tokens,
-                                                     verbose=False)
-feature_names = patentvectorizer.get_feature_names()
-target_names = patentdict['target_names']
-vector_characteristics(vectorized, transformed, feature_names, target_names)
-pp.pca_metric(vectorized, patentdict)
+    # Preprocess and tokenize
+    list_tokens = pp.patent_tokenizer(data_stripped,
+                                      PATH_REPLACE=PATH_REPLACE,
+                                      PATH_SW=PATH_SW)
 
-# Train the classifier
-# clf = MultinomialNB()
-clf = PassiveAggressiveClassifier(C=1)
+    # Vectorize
+    (transformed, patenttransformer,
+     vectorized, patentvectorizer) = pp.tokens_tovectors(list_tokens,
+                                                         verbose=False)
+    feature_names = patentvectorizer.get_feature_names()
+    target_names = patentdict['target_names']
+    pp.vector_characteristics(vectorized, transformed,
+                              feature_names, target_names)
+    pp.pca_metric(vectorized, patentdict)
 
-# Get information on what the classifier learned
-fitted = clf.fit(transformed, categories)
-feature_weights = clf.coef_
-print('****Feature Weights****\n', feature_weights)
-display_important_features(feature_names, target_names, feature_weights)
+    # Train the classifier
+    # clf = MultinomialNB()
+    Cp = [0.01, 0.1, 1.0]
+    clf = SGDClassifier(loss='log', shuffle=True)
 
-# Calculate the classifier metrics
-cv = ShuffleSplit(len(data_stripped), n_iter=10,
-                  test_size=0.7, random_state=42)
-cms, f1_scores = classifier_metrics(transformed, categories, clf, cv)
-classifier_scores(f1_scores)
-output_confusionmatrix(cms, categories, target_names)
+    # Get information on what the classifier learned
+    display_important_features(feature_names, target_names, clf)
+
+    # Calculate the classifier metrics
+    cv = ShuffleSplit(len(data_stripped), n_iter=10,
+                      test_size=0.7, random_state=42)
+    cms, f1_scores = classifier_metrics(transformed, categories, clf, cv)
+    classifier_scores(f1_scores)
+    output_confusionmatrix(cms, categories, target_names)
+
+    # Train Model on full dataset
+    clf.fit(transformed, categories)
+    # Get probablities of each class
+    print('****Class Probabilities****')
+    df_p = probs_class(patentdict, transformed, clf)
+    print(df_p.head())
