7b1b01a05668c4b287fd716147890fd1be28e754
dpmlto1 <philiplessner@kemet.com>
2014-06-03 09:04:11 -0400

    Refactored preprocessing and vectorization into PatentVectorizer class.

--- preprocess.py
+++ preprocess.py
@@ -5,14 +5,14 @@ import io
 import json
 from itertools import cycle
 import numpy as np
+import matplotlib.pyplot as plt
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.datasets import load_files
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import manifold
 from sklearn.decomposition import TruncatedSVD
-import matplotlib.pyplot as plt
 from utils import removeNonAscii, RegexpReplacer
 
 
@@ -33,68 +33,108 @@ def file_tostring(PATH_DATA):
             with io.open(filePath, 'r', encoding='latin_1',
                          errors='replace') as f:
                 raw = f.read()
-                cleaned = removeNonAscii(raw)
-                data.append(cleaned)
+                data.append(raw)
     return data, filenames
 
 
-def patent_totfidf(data, PATH_REPLACE=None, PATH_SW=None):
-    with io.open(PATH_REPLACE, 'r', encoding='utf-8') as f:
-        d = json.load(f)
-    with io.open(PATH_SW, 'r', encoding='utf-8') as f:
-        combined_stops = set([line.strip() for line in f])
+def loadClassifiedData(pathtraining):
+    '''
+    Loads classified data to be used for training or a model
+    or to perform a training/testing split
+    Parameter
+        pathtraining: path to top level directory where
+                      classified data is located
+    Returns:
+        dataDict: {'data': list of strings, one string per document
+                   'target': array of  category labels correspoding to data
+                   'target_names': names of the categories}
+    '''
+    dataDict = load_files(pathtraining,
+                          description=None, categories=None,
+                          load_content=True, shuffle=True,
+                          encoding='latin-1', decode_error='strict',
+                          random_state=0)
+    return dataDict
 
-    def patent_analyzer(document):
+
+class PatentVectorizer(object):
+
+    def __init__(self, data, PATH_REPLACE=None, PATH_SW=None):
+        with io.open(PATH_REPLACE, 'r', encoding='utf-8') as f:
+            d = json.load(f)
+        with io.open(PATH_SW, 'r', encoding='utf-8') as f:
+            combined_stops = set([line.strip() for line in f])
+        english_stops = set(stopwords.words('english'))
+        self.stops = english_stops | combined_stops
+        self.english_stemmer = SnowballStemmer('english')
+        self.data = data
+        replacement_patterns = [(key, value)
+                                for key, value in d[0].items()]
+        self.replacer = RegexpReplacer(patterns=replacement_patterns)
+
+    def _patent_analyzer(self, document):
+        '''
+        Custom analyzer that preprocesses a summarized patent document
+        and tokenizes it.
+        Parameter
+            document: A single string representing a summarized patent document
+        Returns
+            a list of tokens from string
+        '''
+        # Strip non-ascii characters
+        s_acii = removeNonAscii(document)
         # Replacements in string
-        replacement_patterns = [(key, value) for key, value in d[0].items()]
-        clean = RegexpReplacer(patterns=replacement_patterns).replace(document)
+        clean = self.replacer.replace(s_acii)
         # Lower case string
         lower_case = clean.lower()
         # Tokenize string to list of tokens
         tokenized = RegexpTokenizer('[\w]+').tokenize(lower_case)
         # Remove stop words from list of tokens
-        english_stops = set(stopwords.words('english'))
-        stops = english_stops | combined_stops
-        filtered = [token for token in tokenized if token not in stops]
+        filtered = [token for token in tokenized if token not in self.stops]
         # Stem list of tokens
-        english_stemmer = SnowballStemmer('english')
-        return [english_stemmer.stem(token) for token in filtered]
+        return [self.english_stemmer.stem(token) for token in filtered]
 
-    patentvectorizer = CountVectorizer(analyzer=patent_analyzer,
-                                       max_df=0.95,
-                                       decode_error='ignore')
-    vectorized = patentvectorizer.fit_transform(data)
-    patenttransformer = TfidfTransformer()
-    transformed = patenttransformer.fit_transform(vectorized.toarray())
-    vectordict = {'tfidf_vectors': transformed,
-                  'tfidf_instance': patenttransformer,
-                  'count_vectors': vectorized,
-                  'countvector_instance': patentvectorizer,
-                  'feature_names': patentvectorizer.get_feature_names()}
-    return vectordict
+    def patent_totfidf(self):
+        '''
+        Convert summarized patent documents to tf-idf vectors
+        Returns
+            vectordict: {'tfidf_vectors': transformed,
+                         'tfidf_instance': patenttransformer,
+                         'count_vectors': vectorized,
+                         'countvector_instance': patentvectorizer,
+                         'feature_names': patentvectorizer.get_feature_names()}
+        '''
+        patentvectorizer = TfidfVectorizer(analyzer=self._patent_analyzer,
+                                           max_df=0.95,
+                                           decode_error='ignore')
+        vectorized = patentvectorizer.fit_transform(self.data)
+        self.vectordict = {'tfidf_vectors': vectorized,
+                           'tfidf_instance': patentvectorizer,
+                           'feature_names': patentvectorizer.get_feature_names()}
+        return self.vectordict
 
+    def vector_characteristics(self, patentdict):
+        '''
+        Print some vector characteristics
+        '''
+        print('Transformed Shape: ', self.vectordict['tfidf_vectors'].shape)
+        print('Number of Categories: ', len(patentdict['target_names']))
+        print('\n****Target Names***\n', patentdict['target_names'])
+        print('Number of Features:', len(self.vectordict['feature_names']))
+        print('\n****Feature Names****\n', self.vectordict['feature_names'])
 
-def vector_characteristics(patentdict, vectordict):
-    print('Vectorized Shape: ', vectordict['count_vectors'].shape)
-    print('Transformed Shape: ', vectordict['tfidf_vectors'].shape)
-    print('Number of Categories: ', len(patentdict['target_names']))
-    print('\n****Target Names***\n', patentdict['target_names'])
-    print('Number of Features:', len(vectordict['feature_names']))
-    print('\n****Feature Names****\n', vectordict['feature_names'])
+    def pca_metric(self, d):
+        vector_pca = TruncatedSVD(
+            n_components=2).fit_transform(self.vectordict['tfidf_vectors'])
+        colors = ['b', 'g', 'r', 'y', 'k', 'c', 'm']
+        plt.figure(figsize=(10, 10))
+        for i, c in zip(np.unique(d['target']), cycle(colors)):
+            plt.scatter(vector_pca[d['target'] == i, 0],
+                        vector_pca[d['target'] == i, 1],
+                        c=c, label=d['target_names'][i], alpha=1.0)
 
-
-def pca_metric(d, vd):
-    vector_pca = TruncatedSVD(
-        n_components=2).fit_transform(vd['tfidf_vectors'])
-    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
-    plt.figure(figsize=(10, 10))
-    for i, c in zip(np.unique(d['target']), cycle(colors)):
-        plt.scatter(vector_pca[d['target'] == i, 0],
-                    vector_pca[d['target'] == i, 1],
-                    c=c, label=d['target_names'][i], alpha=0.5)
-
-    plt.legend(loc='best')
-    plt.show()
+        plt.legend(loc='best')
+        plt.show()
 
 
 def mds_metric(vectors, plot=True):
