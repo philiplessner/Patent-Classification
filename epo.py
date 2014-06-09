b67ad0ef773d3f7d3db47579e6abfadab3040945
dpmlto1 <philiplessner@kemet.com>
2014-06-08 21:26:02 -0400

    Added Title and Abstract to Description and combined in one file.

--- epo.py
+++ epo.py
@@ -4,17 +4,31 @@ import io
 import re
 import requests
 
-patentn = 'EP2618351'
-query = ''.join(['http://ops.epo.org/3.1/rest-services/published-data/',
-                 'publication/epodoc/', patentn, '/description'])
-headers = {'content-type': 'application/json'}
-r = requests.get(query, headers=headers)
-d = r.json()
 
-t1 = d['ops:world-patent-data']['ftxt:fulltext-documents']
-desc_body = t1['ftxt:fulltext-document']['description']['p']
+def main():
+    patentn = 'EP2618351'
+    headers = {'content-type': 'application/json'}
+    root_query = ''.join(['http://ops.epo.org/3.1/rest-services/',
+                          'published-data/publication/epodoc/', patentn])
+    biblo_query = ''.join([root_query, '/biblio'])
+    desc_query = ''.join([root_query, '/description'])
+    biblio_r = requests.get(biblo_query, headers=headers)
+    biblio_d = biblio_r.json()
+    t0 = biblio_d['ops:world-patent-data']['exchange-documents']
+    title = t0[
+        'exchange-document']['bibliographic-data']['invention-title'][0]['$']
+    abstract = '\n'.join([item['$']
+                          for item in t0['exchange-document']['abstract']['p']])
+    desc_r = requests.get(desc_query, headers=headers)
+    desc_d = desc_r.json()
+    t1 = desc_d['ops:world-patent-data']['ftxt:fulltext-documents']
+    desc_body = t1['ftxt:fulltext-document']['description']['p']
+    description = '\n'.join([re.sub(r'\[\d+\]', '', item['$'])
+                             for item in desc_body])
+    contents = ''.join(['Title\n', title, '\nAbstract\n', abstract,
+                        '\n', description])
+    with io.open('test_epo.txt', 'w', encoding='utf-8') as f:
+        f.write(contents)
 
-with io.open('test_desc.txt', 'w', encoding='utf-8') as f:
-    for item in desc_body:
-        item_cleaned = re.sub(r'\[\d+\]', '', item['$'])
-        f.write(item_cleaned + '\n')
+if __name__ == '__main__':
+    main()
